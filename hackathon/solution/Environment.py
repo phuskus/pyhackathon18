87eab4e58602c940b93a6ae2afbf3008ccacef68
DjukaBazuka <44606365+DjukaBazuka@users.noreply.github.com>
2018-11-17 22:46:31 +0100

    Promenio sam environment i vratio solution worker-a na ono sto smo imali od njih

--- hackathon/solution/Environment.py
+++ hackathon/solution/Environment.py
@@ -1,50 +1,84 @@
 import random
-from hackathon.utils.utils import DataMessage
+from hackathon2018.hackathon.utils.utils import DataMessage
 
 class Environment:
-    def __init(self):
-        self.state = None
-        '''STATUS -- [load1_Status, load2_Status, load3_Status, solar_Status]'''
+    def __init__(self):
+        self._state_count = 1
+        self._state = None
+        self.consumers = []
         self.status = [True, True, True, True]
+        self.battery = 0
 
-    def update(self, DataMsg : DataMessage):
-        self.state = DataMsg
+    def get_state(self):
+        return self._state
 
-    def getState(self):
-        '''INPUT -- grid_Status, bess_SOC, solar_production'''
-        return [self.state.grid_status,
-                self.state.bessSOC,
-                self.state.solar_production]
+    def setState(self, msg: DataMessage):
+        self.state = msg
+        self.consumers = [self.state.current_load * 0.2, self.state.current_load * 0.4, self.state.current_load * 0.4]
+        self.battery = self.state.bessSOC * self.state.bessPower
 
-    def penal(self, action):
-        '''action [load1_State, load2_State, load3_State, bess_PowerReference, solar_StateCoef]'''
+    def next_state(self, action):
+        if self.state.grid_status:
+            "Ako je grid ukljucen imas posebnu politiku odlucivanja"
 
-        penal = 0
+            "obracunaj koliko snage imas na raspolaganju"
+            powerAtDisposal = sum([self.state.mainGridPower, self.state.solar_production, self.state.bessPower])
 
-        i = 0
-        if action[i] < 0.5 and self.status[i]:
-            self.status[i] = False
-            penal -= 20
-        elif action[i] < 0.5 and not self.status[i]:
-            penal -= 1
-        elif action[i] >= 0.5:
-            self.status[i] = True
+            "izracunaj koliko snage potrosaci zahtevaju"
+            powerDemand = sum(self.consumers)
+            penalty = [0,0,0,0,0]
 
-        i = 1
-        if action[i] < 0.5 and self.status[i]:
-            self.status[i] = False
-            penal -= 4
-        elif action[i] < 0.5 and not self.status[i]:
-            penal -= 0.4
-        elif action[i] >= 0.5:
-            self.status[i] = True
+            if powerDemand > 0:
+                if action[0] * powerAtDisposal < self.consumers[0]:
+                    if self.status[0]:
+                        "Ako smatram da treba da iskljucim prvog potrosaca"
+                        penalty[0] += 20
+                        self.status[0] = False
+                    else:
+                        "Ako mi je prvi potrosac vec bio iskljucen i sada zelim da ga takvog ostavim"
+                        penalty[0] += 1
+                else:
+                    "Ako ne smatram da treba da iskljucim prvog potrosaca"
+                    if not self.status[0]:
+                        "Ako je prvi potrosac bio iskljucen opet ga samo upalim"
+                        self.status[0] = True
+                    "ako sam ukljucio prvog potrosaca, smanjujem energiju na raspolaganju za druga dva potrosaca"
+                    powerAtDisposal -= self.consumers[0]
+                if action[1] * powerAtDisposal < self.consumers[1]:
+                    if self.status[0]:
+                        "Ako smatram da treba da iskljucim drugog potrosaca"
+                        penalty[1] += 4
+                        self.status[1] = False
+                    else:
+                        "Ako mi je drugi potrosac vec bio iskljucen i sada zelim da ga takvog ostavim"
+                        penalty[1] += 0.4
+                else:
+                    "Ako ne smatram da treba da iskljucim drugog potrosaca"
+                    if not self.status[1]:
+                        "Ako je drugi potrosac bio iskljucen opet ga samo upalim"
+                        self.status[1] = True
+                    "ako sam ukljucio drugog potrosaca, smanjujem energiju na raspolaganju za treceg potrosaca"
+                    powerAtDisposal -= self.consumers[1]
 
-        i = 2
-        if action[i] < 0.5 and self.status[i]:
-            self.status[i] = False
-        elif action[i] < 0.5 and not self.status[i]:
-            penal -= 0.3
-        elif action[i] >= 0.5:
-            self.status[i] = True
+                if action[2] * powerAtDisposal < self.consumers[2]:
+                    if self.status[2]:
+                        "Ako smatram da treba da iskljucim treceg potrosaca"
+                        self.status[2] = False
+                    else:
+                        "Ako mi je treci potrosac vec bio iskljucen i sada zelim da ga takvog ostavim"
+                        penalty[2] += 0.3
+                else:
+                    "Ako ne smatram da treba da iskljucim treceg potrosaca"
+                    if not self.status[2]:
+                        "Ako je treci potrosac bio iskljucen opet ga samo upalim"
+                        self.status[2] = True
+                    "ako sam ukljucio treceg potrosaca, smanjujem energiju na raspolaganju"
+                    powerAtDisposal -= self.consumers[1]
 
-        return penal
+            #if powerAtDisposal > 0:
+
+
+
+
+
+
--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -4,74 +4,44 @@ from hackathon.utils.control import Control
 from hackathon.utils.utils import ResultsMessage, DataMessage, PVMode, \
     TYPHOON_DIR, config_outs
 from hackathon.framework.http_server import prepare_dot_dir
-
-from hackathon.solution.Environment import Environment
-from hackathon.solution.Network import Network
-from hackathon.solution.ReplayMemory import ReplayMemory, Transition
-import torch.optim as optim
-import torch
-import torch.nn.functional as F
-
+from hackathon.solution import Environment
 
 env = Environment()
-memory = ReplayMemory(10000)
 
-device = torch.device("cpu")
-agent1 = Network().to(device)
-agent2 = Network().to(device)
+def worker(msg: DataMessage) -> ResultsMessage:
+    """TODO: This function should be implemented by contestants."""
+    # Details about DataMessage and ResultsMessage objects can be found in /utils/utils.py
 
-agent2.load_state_dict(agent1.state_dict())
-agent2.eval()
-optimizer = optim.RMSprop(agent1.parameters())
+    load_one = True
+    load_two = True
+    load_three = True
+    power_reference = 0.0
+    pv_mode = PVMode.ON
 
+    if msg.grid_status:
+        if msg.bessSOC != 1 and msg.buying_price == msg.selling_price \
+                 and msg.current_load <8:
+             if msg.bessSOC > 0.45:
+                 power_reference = -5.0
+        elif msg.current_load > 8:
+             power_reference = msg.current_load
 
-def optimize_model(batch_size=1000, gamma=0.9):
-    if len(memory) < batch_size:
-        return
-    transitions = memory.sample(batch_size)
-    batch = Transition(*zip(*transitions))
-    state_batch = torch.cat(batch.state)
-    next_states = torch.cat(batch.next_state)
-    reward_batch = torch.cat(batch.reward)
-
-    state_action_values = agent1(state_batch)
-    #next_state_values = torch.zeros(batch_size, device=device)
-    next_state_values = agent2(next_states)
-
-    expected_state_action_values = (next_state_values * gamma) + reward_batch
-    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.detach())
-
-    optimizer.zero_grad()
-    loss.backward()
-    for param in agent1.parameters():
-        param.grad.data.clamp_(-1, 1)
-    optimizer.step()
-
-    return loss.item()
-
-
-def worker(msg: DataMessage):
-
-    #Primi stanje okruzenja
-    env.update(msg)
-    #Smisli odluku (neuronska mreza)
-    action = agent1(env.getState())
-    #Odredi penal i zapamti
-    penalty = env.penal(action)
-    memory.push(env.getState(), action, penalty)
-
-    loss = optimize_model()
-
-    #Odradi odluku
-    load_one = env.status[0]
-    load_two = env.status[1]
-    load_three = env.status[2]
-    power_reference = action[3]
-    if env.status[3]:
-        pv_mode = PVMode.ON
     else:
-        pv_mode = PVMode.OFF
+         if msg.bessSOC == 1 and msg.solar_production > 0:
+             if msg.solar_production < msg.current_load:
+                 power_reference = msg.current_load
+                 load_three = False
+                 pv_mode = PVMode.OFF
+             else:
+                 power_reference = msg.current_load
+         elif msg.bessSOC != 1:
+             load_three = False
+             pv_mode = PVMode.ON
 
+         elif msg.selling_price == msg.buying_price and msg.solar_production == 0:
+             power_reference = msg.current_load
+
+    # Dummy result is returned in every cycle here
     return ResultsMessage(data_msg=msg,
                           load_one=load_one,
                           load_two=load_two,
@@ -80,10 +50,10 @@ def worker(msg: DataMessage):
                           pv_mode=pv_mode)
 
 
-
 def run(args) -> None:
     prepare_dot_dir()
     config_outs(args, 'solution')
+
     cntrl = Control()
 
     for data in cntrl.get_data():
