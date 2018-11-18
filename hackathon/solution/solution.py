04751a0f55432722e6c83ccd7da93df5f1d5c63e
Stefan Stegić <stefanstegic@gmail.com>
2018-11-18 15:49:37 +0100

    Heuristika 4

--- hackathon/solution/solution.py
+++ hackathon/solution/solution.py
@@ -6,25 +6,23 @@ from hackathon.utils.utils import ResultsMessage, DataMessage, PVMode, \
 from hackathon.framework.http_server import prepare_dot_dir
 
 prev_buyingPrice = 0
-failsafe_percentage = 0.3
+failsafe_percentage = 0.35
 high_priority_failsafe_percentage = 0.25
 bessCap = 1200.0 # Expressed in kW * minutes
-prev_load = []
-prev_load[0] = [True, 0.2]
-prev_load[1] = [True, 0.4]
-prev_load[2] = [True, 0.4]
-number_of_consumers = 3
-lowest_activated_priority = 2
-
+prev_load_one = True
+prev_load_two = True
+prev_load_three = True
 def clamp(val, minv, maxv):
     return max(min(val, maxv), minv)
 
 def worker(msg: DataMessage) -> ResultsMessage:
     global prev_buyingPrice, failsafe_percentage, high_priority_failsafe_percentage, bessCap
-    global load, lowest_activated_priority, prev_load, number_of_consumers
+    global prev_load_one, prev_load_two, prev_load_three
 
     #Init vars
-    load = [True] * 3
+    load_one = True
+    load_two = True
+    load_three = True
     power_reference = 0.0
     pv_mode = PVMode.ON
 
@@ -33,7 +31,12 @@ def worker(msg: DataMessage) -> ResultsMessage:
         #Grid is ONLINE
 
         # All consumers on
+        load_one = True
+        load_two = True
+        load_three = True
+
         # Solar panel on
+        pv_mode = PVMode.ON
 
         # DAY, EXPENSIVE BUYING PRICE - check if buyingPrice > prev_buyingPrice AND prev_buyingPrice <= sellingPrice
         if msg.buying_price > prev_buyingPrice and prev_buyingPrice <= msg.selling_price:
@@ -55,93 +58,106 @@ def worker(msg: DataMessage) -> ResultsMessage:
     else:
         #GRID IS OFFLINE
         #print("BLACKOUT (msgid: " + str(msg.id) + ")")
+        # Consumer three off
+        load_one = True
+        load_two = True
+        load_three = False
 
+        coef = 1
+        if prev_load_three:
+            coef -= 0.4
+        if prev_load_two:
+            coef -= 0.4
+        if prev_load_one:
+            coef -= 0.2
 
-        '''If power at disposal is not enough to satisfy power required, kill the lowest priority load, and check again'''
+        power_required = msg.current_load * coef - msg.solar_production
+        #print("--Init p.req: " + str(power_required))
+        # If solar is enough to satisfy consumption (negative power_required), excess power charges bess ---> Prevent overcharging bess
+        # If solar is NOT enough to satisfy consumption (positive power required), help with bess
         kWminutes_left_in_bess = bessCap * msg.bessSOC
-        max_draw = 5.0  # kW
-        bess_power_at_disposal = clamp(kWminutes_left_in_bess, 0, 5)
-        current_load = msg.current_load
-        while True:
-            solar_production = msg.solar_production
-            if pv_mode == PVMode.OFF:
-                solar_production = 0
+        max_draw = 5.0 #kW
+        #print("--kWm left: " + str(kWminutes_left_in_bess))
 
-            power_at_disposal = solar_production + bess_power_at_disposal
-            power_required = current_load
-            if power_at_disposal >= power_required:
-                '''If solar power is enough to satisfy consumption, send excess to charge Bess'''
-                if solar_production >= power_required:
-                    power_reference = power_required - solar_production
-                    if power_reference < -5:
-                        '''Kill solar panel so as not to overcharge Bess'''
-                        pv_mode = PVMode.OFF
-                        continue
-                    newSOC = msg.bessSOC + power_reference/bessCap
-                    if newSOC > 1:
-                        '''Kill solar panel so as not to overcharge Bess'''
-                        pv_mode = PVMode.OFF
-                        continue
-                    break
-                elif solar_production < power_required:
-                    '''If solar is not enough, substitute power from Bess (if she be capable!)'''
-                    power_reference = power_required - solar_production
-                    if power_reference > 5:
-                        '''Kill the lowest pri load'''
-                        load[lowest_activated_priority] = False
-                        if lowest_activated_priority == 2:
-                            current_load = current_load * 0.6
-                        if lowest_activated_priority == 1:
-                            beginning_current_load = 10 / 6 * current_load
-                            current_load = beginning_current_load - 2 * 0.4 * beginning_current_load
-                        if lowest_activated_priority == 0:
-                            current_load = 0
+        if power_required > kWminutes_left_in_bess:
+            #print("--1")
+            # Kill load_two, leave load_one
+            load_two = False
+            # Recalc power required and assess
+            coef = 1
+            if prev_load_two:
+                coef = 0.33
+            power_required = msg.current_load * coef - msg.solar_production
+            #print("----p.req: " + str(power_required))
+            if power_required <= kWminutes_left_in_bess and power_required <= max_draw:
+                # Ok
+                power_reference = power_required
+            else:
+                # No power left, kill one
+                load_one = False
 
-                        lowest_activated_priority -= 1
-                        if lowest_activated_priority < 0:
-                            break
-                        continue
-                    newSOC = msg.bessSOC - power_reference/bessCap
+        elif power_required >= 0 and power_required <= kWminutes_left_in_bess and power_required <= max_draw:
+            #print("--2")
+            power_reference = power_required
 
-                    if newSOC < 0:
-                        '''Kill the lowest pri load'''
-                        load[lowest_activated_priority] = False
-                        if lowest_activated_priority == 2:
-                            current_load = current_load * 0.6
-                        if lowest_activated_priority == 1:
-                            beginning_current_load = 10/6 * current_load
-                            current_load = beginning_current_load - 2 * 0.4 * beginning_current_load
-                        if lowest_activated_priority == 0:
-                            current_load = 0
+        elif power_required >= 0 and power_required <= kWminutes_left_in_bess and power_required > max_draw:
+            # Kill load_two, leave load_one
+            load_two = False
+            # Recalc power required and assess
+            coef = 1
+            if prev_load_two:
+                coef = 0.33
+            power_required = msg.current_load * coef - msg.solar_production
+            # print("----p.req: " + str(power_required))
+            if power_required <= kWminutes_left_in_bess and power_required <= max_draw:
+                # Ok
+                power_reference = power_required
+            else:
+                # No power left, kill one
+                load_one = False
+                power_reference = 0
 
-                        lowest_activated_priority -= 1
-                        if lowest_activated_priority < 0:
-                            break
-                        continue
-                    break
+        elif power_required < 0:
+            #print("--3")
+            # If there's more than 5 kW's for Bess, kill solar
+            if power_required < -5:
+                pv_mode = PVMode.OFF
+                # Supply power solely from bess
+                # Kill consumers if power_required > 5
+                power_required += msg.solar_production
+                #print("----p.req: " + str(power_required))
+                if power_required > kWminutes_left_in_bess:
+                    # Kill load_two, leave load_one
+                    load_two = False
+                    # Recalc power required and assess
+                    coef = 1
+                    if prev_load_two:
+                        coef = 0.33
+                    power_required = msg.current_load * coef - msg.solar_production
+                    if power_required <= kWminutes_left_in_bess and power_required <= max_draw:
+                        # Ok
+                        power_reference = power_required
+                    else:
+                        # No power left, kill one
+                        load_one = False
+                        power_reference = 0
 
-            elif power_at_disposal < power_required:
-                '''Kill the lowest priority load'''
-                load[lowest_activated_priority] = False
-                if lowest_activated_priority == 2:
-                    current_load = current_load * 0.6
-                if lowest_activated_priority == 1:
-                    beginning_current_load = 10 / 6 * current_load
-                    current_load = beginning_current_load - 2 * 0.4 * beginning_current_load
-                if lowest_activated_priority == 0:
-                    current_load = 0
+                elif power_required <= kWminutes_left_in_bess and power_required <= max_draw:
+                    power_reference = power_required
+        #print("END BLACKOUT")
+        #print("    load_one: " + str(load_one))
+        #print("    load_two: " + str(load_two))
+        #print("    bess p.ref: " + str(power_reference))
+        #print("    pv_mode: " + str(pv_mode))
 
-                lowest_activated_priority -= 1
-                if lowest_activated_priority < 0:
-                    break
+    prev_load_one = load_one
+    prev_load_two = load_two
+    prev_load_three = load_three
 
-    prev_load[0] = load[0]
-    prev_load[1] = load[1]
-    prev_load[2] = load[2]
     return ResultsMessage(data_msg=msg,
-                          load_one=load[0],
-                          load_two=load[1],
-                          load_three=load[2],
+                          load_one=load_one,
+                          load_two=load_two,
+                          load_three=load_three,
                           power_reference=power_reference,
                           pv_mode=pv_mode)
 
